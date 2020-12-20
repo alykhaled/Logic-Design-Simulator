37e119b17d7c8c11ebb10e9d3ca1794b04f4d69e
Aly Khaled <alykhaled2001@live.com>
2020-12-20 12:23:12 +0200

    Add project files.

---
+++ GUI/Output.cpp
@@ -0,0 +1,328 @@
+#include "Output.h"
+
+Output::Output()
+{
+	//Initialize user interface parameters
+
+	UI.AppMode = DESIGN;	//Design Mode is the startup mode
+
+	//Initilaize interface colors
+	UI.DrawColor = BLACK;
+	UI.SelectColor = BLUE;
+	UI.ConnColor = RED;
+	UI.MsgColor = BLACK;
+	UI.BkGrndColor = WHITE;
+
+	//Create the drawing window
+	pWind = CreateWind(UI.width, UI.height, UI.wx, UI.wy);
+	ChangeTitle("Programming Techniques Project");
+
+	CreateDesignToolBar();	//Create the desgin toolbar
+	CreateStatusBar();		//Create Status bar
+	ClearDrawingArea();
+	pWind->SetFont(20, BOLD | ITALICIZED, BY_NAME, "Arial");
+	pWind->SetPen(UI.MsgColor);
+	pWind->DrawImage("images\\Menu\\modetoolbar.jpg", 0, UI.ToolBarHeight, 1500, 30);
+}
+
+Input* Output::CreateInput() const
+{
+	//Create an Input Object & assign it to the Same Window
+	Input* pIn = new Input(pWind);
+	return pIn;
+}
+
+//======================================================================================//
+//								Interface Functions										//
+//======================================================================================//
+
+window* Output::CreateWind(int wd, int h, int x, int y) const
+{
+	return new window(wd, h, x, y);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::ChangeTitle(string Title) const
+{
+	pWind->ChangeTitle(Title);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::CreateStatusBar() const
+{
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.height - UI.StatusBarHeight, UI.width, UI.height - UI.StatusBarHeight);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::PrintMsg(string msg) const
+{
+	ClearStatusBar();	//Clear Status bar to print message on it
+	// Set the Message offset from the Status Bar
+	int MsgX = 25;
+	int MsgY = UI.StatusBarHeight - 10;
+
+	// Print the Message
+	pWind->SetFont(20, BOLD | ITALICIZED, BY_NAME, "Arial");
+	pWind->SetPen(UI.MsgColor);
+	pWind->DrawString(MsgX, UI.height - MsgY, msg);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::ClearStatusBar()const
+{
+	// Set the Message offset from the Status Bar
+	int MsgX = 25;
+	int MsgY = UI.StatusBarHeight - 10;
+
+	//Overwrite using bachground color to erase the message
+	pWind->SetPen(UI.BkGrndColor);
+	pWind->SetBrush(UI.BkGrndColor);
+	pWind->DrawRectangle(MsgX, UI.height - MsgY, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//Clears the drawing (degin) area
+void Output::ClearDrawingArea() const
+{
+	pWind->SetPen(RED, 1);
+	pWind->SetBrush(WHITE);
+	pWind->DrawRectangle(0, UI.ToolBarHeight, UI.width, UI.height - UI.StatusBarHeight);
+	pWind->SetPen(GRAY, 1);
+	for (int i = 80; i < 700; i += 50)
+	{
+		pWind->DrawLine(0, i, 1500, i);
+
+	}
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearToolBar() const
+{
+	pWind->SetPen(WHITE, 1);
+	pWind->SetBrush(WHITE);
+	pWind->DrawRectangle(0, 0, 1500, UI.ToolBarHeight);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//Draws the menu (toolbar) in the Design mode
+void Output::CreateDesignToolBar() const
+{
+	ClearToolBar();
+	UI.AppMode = DESIGN;	//Design Mode
+
+	//You can draw the tool bar icons in any way you want.
+
+	//First prepare List of images for each menu item
+	string MenuItemImages[ITM_DSN_CNT];
+	MenuItemImages[ITM_AND2] = "images\\Menu\\Menu_AND2.jpg";
+	MenuItemImages[ITM_OR2] = "images\\Menu\\Menu_OR2.jpg";
+	MenuItemImages[ITM_NAND2] = "images\\Menu\\Menu_NAND2.jpg";
+	MenuItemImages[ITM_CONNECTION] = "images\\Menu\\wire.jpg";
+	//MenuItemImages[ITM_SIMULATION] = "images\\Menu\\simulation.jpg";
+
+	MenuItemImages[ITM_EXIT] = "images\\Menu\\Menu_Exit.jpg";
+
+	//TODO: Prepare image for each menu item and add it to the list
+
+	//Draw menu item one image at a time
+	for (int i = 0; i < 4; i++)
+		pWind->DrawImage(MenuItemImages[i], i * UI.ToolItemWidth, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+
+
+	//Draw a line under the toolbar
+	pWind->SetPen(BLUE, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//Draws the menu (toolbar) in the simulation mode
+void Output::CreateSimulationToolBar() const
+{
+	ClearToolBar();
+	UI.AppMode = SIMULATION;	//Simulation Mode
+
+	//You can draw the tool bar icons in any way you want.
+
+	//First prepare List of images for each menu item
+	string MenuItemImages[ITM_SIM_CNT];
+
+	MenuItemImages[ITM_SIM] = "images\\Simulation toolbar\\SIM_SIMULATE.jpg";
+	MenuItemImages[ITM_TRUTH] = "images\\Simulation toolbar\\SIM_TRUTH_TABLE.jpg";
+	MenuItemImages[ITM_Change_Switch] = "images\\Simulation toolbar\\SIM_SWITCH.jpg";
+	MenuItemImages[ITM_SELECT] = "images\\Simulation toolbar\\SIM_SELECT.jpg";
+	MenuItemImages[ITM_DEL] = "images\\Simulation toolbar\\SIM_DELETE.jpg";
+	MenuItemImages[ITM_MOVE] = "images\\Simulation toolbar\\SIM_MOVE.jpg";
+	MenuItemImages[ITM_SAVE] = "images\\Simulation toolbar\\SIM_SAVE.jpg";
+	MenuItemImages[ITM_LOAD] = "images\\Simulation toolbar\\SIM_LOAD.jpg";
+	MenuItemImages[ITM_UNDO] = "images\\Simulation toolbar\\SIM_UNDO.jpg";
+	MenuItemImages[ITM_REDO] = "images\\Simulation toolbar\\SIM_REDO.jpg";
+
+	//Draw menu item one image at a time
+	for (int i = 0; i < ITM_SIM_CNT; i++)
+		pWind->DrawImage(MenuItemImages[i], i * UI.ToolItemWidth, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);
+
+}
+
+//======================================================================================//
+//								Components Drawing Functions							//
+//======================================================================================//
+
+void Output::DrawAND2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)	//use image in the highlighted case
+		GateImage = "Images\\Gates\\Gate_AND2_Hi.jpg";
+	else
+		GateImage = "Images\\Gates\\Gate_AND2.jpg";
+
+	//Draw AND2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+
+}
+void Output::DrawOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Images\\Gates\\Gate_OR2_Hi.jpg";
+	else
+		GateImage = "Images\\Gates\\Gate_NOT.jpg";
+
+	//Draw Or2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.NOT_Width, UI.NOT_Height);
+}
+void Output::DrawINV(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Images\\Gates\\Gate_NOT2_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_NOT2.jpg";
+	//Draw INV gate at Gfx_Info (2nd corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawBuff(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_Buff_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_Buff.jpg";
+	//Draw Buff. at Gfx_Info (3rd corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawNAND2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_NAND2_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_NAND2.jpg";
+	//Draw NAND2 Gate at Gfx_Info (4th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawNOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_NOR2_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_NOR2.jpg";
+	//Draw NORR2 Gate at Gfx_Info (5th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawXOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_XOR2_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_XOR2.jpg";
+	//Draw XORR2 Gate at Gfx_Info (7th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawXNOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_XNOR2_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_XNOR2.jpg";
+	//Draw XNOR2 Gate at Gfx_Info (8th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawAND3(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_XAND2_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_XAND2.jpg";
+	//Draw AND3 Gate at Gfx_Info (6th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawNOR3(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_NOR3_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_NOR3.jpg";
+	//Draw NOR3 Gate at Gfx_Info (6th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawXOR3(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_XOR3_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_XNOR3.jpg";
+	//Draw XOR3 Gate at Gfx_Info (6th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawSwitch(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_Switch_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_Switch.jpg";
+	//Draw Switch at Gfx_Info (6th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawLED(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if (selected)
+		GateImage = "Image\\Gates\\Gate_LED_Hi.jpg";
+	else
+		GateImage = "Image\\Gates\\Gate_LED.jpg";
+	//Draw LED at Gfx_Info (6th corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+void Output::DrawConnection(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	//TODO: Add code to draw connection
+	pWind->SetPen(BLACK, 2);
+
+	pWind->DrawLine(r_GfxInfo.x1, r_GfxInfo.y1, r_GfxInfo.x2, r_GfxInfo.y1);
+	pWind->DrawLine(r_GfxInfo.x2, r_GfxInfo.y1, r_GfxInfo.x2, r_GfxInfo.y2);
+
+
+}
+
+Output::~Output()
+{
+	delete pWind;
+}
---
+++ GUI/UI_Info.h
@@ -0,0 +1,42 @@
+#ifndef UI_INFO_H
+#define UI_INFO_H
+
+#include "..\CMUgraphicsLib\CMUgraphics.h"
+#include "..\Defs.h"
+
+//User Interface information file.
+//This file contains info that is needed by Input and Output classes to
+//handle the user interface
+
+__declspec(selectany) //This line to prevent "redefinition error"
+
+struct UI_Info	//User Interface Info.
+{
+	MODE AppMode;		//Application Mode (design or simulation)
+
+	static const int	width = 1500, height = 780,	//Window width and height
+		wx = 15, wy = 15,			//Window starting coordinates
+		StatusBarHeight = 50,	//Status Bar Height
+		ToolBarHeight = 80,		//Tool Bar Height (distance from top of window to bottom line of toolbar)
+		ToolItemWidth = 150;		//Width of each item in toolbar menu
+
+	color DrawColor;		//Drawing color
+	color SelectColor;		//Highlighting color
+	color ConnColor;		//Connector color
+	color MsgColor;			//Messages color
+	color BkGrndColor;		//Back ground color
+
+
+	//This should be calculated accurately because it will be used later to create connections between gates
+	//For now, we will assume that rect width = 50 and height = 50
+	static const int	AND2_Width = 83,		//AND2 Gate Image default width
+		AND2_Height = 50,		//AND2 Gate Image default height
+		NOT_Width = 92,			//NOT Gate Image default width
+		NOT_Height = 50,
+		NAND2_Width = 87,		//NAND2 Gate Image default width
+		NAND2_Height = 50,		//NAND2 Gate Image default height
+		OR_Width = 92,			//OR Gate Image default width
+		OR_Height = 50;		//OR Gate Image default height
+}UI;	//create a single global object UI
+
+#endif
